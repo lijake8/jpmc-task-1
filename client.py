b32e5df5a89d13362e067af1bdc2bcfedf2ba9ff
ljake8 <lijake@umich.edu>
2022-10-18 13:20:37 -0400

    finished task 1 by updating getDataPoint, getRatio, and main

--- client.py
+++ client.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices = defaultdict(float)
+        prices = defaultdict(float) #defaultdict negates need to search for key first
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock] = price
